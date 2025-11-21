1b9ac4e78c9a5ecfa5a24fa45b3ce6471c1d7ca9
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2025-11-21 08:00:50 +0100

    rrML by Pokar, fixed Euler. Singly diffusional

--- generate_images.py
+++ generate_images.py
@@ -318,7 +318,8 @@ def edm_sampler(
 
         random_diffusion = randn_like(x_cur) * beta
 
-        x_next = x_hat + (t_next - t_hat) * epsilon_predictor_cur + random_diffusion
+        #x_next = x_hat + (t_next - t_hat) * epsilon_predictor_cur + random_diffusion
+        x_next = r_val * x_hat + (1 - r_val) * x_predictor_cur
         # x_next = t_next/t_hat * x_hat + (1 - t_next/t_hat) * denoised  # eqivalently
         # Explicit Euler update: move from σ = t_hat down to the scheduled next σ = t_next using slope d_cur.
 
