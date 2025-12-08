dc2b57b7b48d4d78e9f1cd2a97d344a39e335abe
SzymonNowakowski <szymon.nowakowski@gmail.com>
2025-12-08 19:01:51 +0100

    rewritten rr block with a new time-dependant function. Constant FLOW and ML quotients. FLOW Pokarized Heun, 32 steps, 63 NFEs

--- generate_images.py
+++ generate_images.py
@@ -343,8 +343,8 @@ def edm_sampler(
       0.99998943, 0.99999745
     ]), dtype, noise.device)
 
-    betas_diffusion = betaML
-    r_vals = rrML
+    betas_diffusion = betaFLOW
+    r_vals = rrFLOW
 
     # >>>>>>>>>>>>>>>>>>>>>>> END: Alternative schedule block <<<<<<<<<<<<<<<<<<<<<<<<<
 
