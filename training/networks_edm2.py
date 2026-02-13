32 uniform steps, euler, fixing the input to F with new c_in and c_noise, renaiming x to z in the numerical velocity-integrator for clarity of what happens and why variances (reflected by c_in, c_noise) are what they are.

training/networks_edm2.py
@@ -335,20 +335,23 @@ class Precond(torch.nn.Module):
         b = 1 / sqrt_1_plus_r_sqr
         return sigma.to(torch.float32), r.to(torch.float32), a.to(torch.float32), b.to(torch.float32)
 
-    def velocity(self, x, t, class_labels=None, log_r=logr_normal, dlogr_dt = dlogr_dt_normal, force_fp32=False, return_logvar=False, **unet_kwargs):
-        x = x.to(torch.float32)
+    def velocity(self, z, t, class_labels=None, log_r=logr_normal, dlogr_dt = dlogr_dt_normal, force_fp32=False, return_logvar=False, **unet_kwargs):
+        z = z.to(torch.float32)
         t = t.to(torch.float64).reshape(-1, 1, 1, 1)
         sigma, r, a, b = self.t2stats(t, log_r=log_r)  #already in float32
 
-        class_labels = None if self.label_dim == 0 else torch.zeros([1, self.label_dim], device=x.device) if class_labels is None else class_labels.to(torch.float32).reshape(-1, self.label_dim)
-        dtype = torch.float16 if (self.use_fp16 and not force_fp32 and x.device.type == 'cuda') else torch.float32
+        class_labels = None if self.label_dim == 0 else torch.zeros([1, self.label_dim], device=z.device) if class_labels is None else class_labels.to(torch.float32).reshape(-1, self.label_dim)
+        dtype = torch.float16 if (self.use_fp16 and not force_fp32 and z.device.type == 'cuda') else torch.float32
 
         # Preconditioning weights.
-        c_in = 1 / (self.sigma_data ** 2 + sigma ** 2).sqrt()
-        c_noise = sigma.flatten().log() / 4
+        # c_in = 1 / (self.sigma_data ** 2 + sigma ** 2).sqrt() # original EDM2 line
+        c_in = 1 / self.sigma_data                              # the variance scaling for the Z_t which has a constant variance of sigma_data ^ 2
+
+        # c_noise = sigma.flatten().log() / 4                 # original EDM2 line
+        c_noise = -log_r(t) / 4 # the effective noise is b/a = 1/r which, after gets log and scaled by 1/4, becomes -log_r(t) / 4
 
         # Run the model.
-        x_in = (c_in * x).to(dtype)
+        x_in = (c_in * z).to(dtype)
         F_x = self.unet(x_in, c_noise, class_labels, **unet_kwargs)
         velocity = dlogr_dt(t) * a * b * self.sigma_data * F_x.to(torch.float32)
 